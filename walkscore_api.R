08bf3b598327d52d3edd922618061c47e99dffaf
benjaminrobinson <benj.robinson2@gmail.com>
2015-04-26 08:59:01 -0400

    Updated Script and US Libraries Data

--- walkscore_api.R
+++ walkscore_api.R
@@ -1,25 +1,31 @@
 #install.packages("jsonlite")
-#install.packages("curl")
 library(jsonlite)
-library(curl)
-setwd("C:/Users/Benjamin/Desktop/DC Stuff/walkscore_api_json_converstion
-")
+library(ggmap)
+setwd("C:/Users/Benjamin/Desktop/DC Stuff/walkscore_api_json_converstion")
 
-dc_lib <- read.csv("dc_libraries.csv")
-dc_lib$unique_id <- paste0(dc_lib$FSCSKEY,dc_lib$FSCS_SEQ)
+us_lib <- read.csv("us_libraries.csv",stringsAsFactors=FALSE)
+dc_lib <- us_lib[which(us_lib$STABR=='DC'),]
+cin_lib <- us_lib[which(us_lib$CITY=='CINCINNATI'),]
+
+##CHUNK 5000 address per file
+ws_1 <- us_lib[1:5000,]
+ws_2 <- us_lib[5001:10000,]
+ws_3 <- us_lib[10001:15000,]
+ws_4 <- us_lib[15001:17586,]
 
 test <- data.frame()
-for (i in unique(dc_lib$unique_id)){
-	lib_id <- subset(dc_lib,unique_id==i)
-	lat <- lib_id$LATITUDE[1]
-	lon <- lib_id$LONGITUDE[1]
-	address <- lib_id$ADDRESS[1]
-	city <- lib_id$CITY[1]
-	zip <- lib_id$ZIP[1]
-	county <- lib_id$CNTY[1]
-	lib_name <- lib_id$LIBNAME[1]
+for (b in unique(dc_lib$LIBID)){
+	lib_code <- subset(dc_lib,LIBID==b)
+	lat <- lib_code$LATITUDE[1]
+	lon <- lib_code$LONGITUDE[1]
+	address <- lib_code$ADDRESS[1]
+	city <- lib_code$CITY[1]
+	state <- lib_code$STABR[1]
+	zip <- lib_code$ZIP[1]
+	county <- lib_code$CNTY[1]
+	lib_name <- lib_code$LIBNAME[1]
 
-	ws <- as.data.frame(fromJSON(paste0("http://api.walkscore.com/score?format=json&lat=",a$snapped_lat,"&lon=",a$snapped_lon,"&wsapikey=<INSERT KEY HERE>")))
+	ws <- fromJSON(paste0("http://api.walkscore.com/score?format=json&lat=",lat,"&lon=",lon,"&wsapikey=<INSERT KEY HERE>"))
 	
 	walkscore <- ws$walkscore
 	description <- ws$description
@@ -27,3 +33,5 @@ for (i in unique(dc_lib$unique_id)){
 	new_row <- cbind(lat,lon,address,city,zip,county,lib_name,walkscore,description)
 	test <- rbind(test,new_row)
 }
+
+##MAP TIME!
